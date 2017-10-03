eb5b48a5b7d2bd1b63b1f3cac4366cb741b7f863
Nikita Antonenkov <krigar1184@gmail.com>
2017-10-03 10:25:23 +0300

    Tests

--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
 from .app import app
 from flask import render_template, request
 from app.forms import DanifyForm
+from app.core import danify
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = DanifyForm(request.data)
+    form.validate_on_submit()
 
     if request.method == 'POST':
-        return render_template('result.html', result=form.text)
+        return render_template('result.html', result=danify(form.text))
 
     return render_template('form.html', form=form)
